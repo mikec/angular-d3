8c8354681e2a83130ac290a8cd04325e0af45bf6
Mike Calvanese <mfcalvanese@gmail.com>
2014-04-01 01:56:20 -0400

    fixed line transitions

--- src/multiline/multiline.js
+++ src/multiline/multiline.js
@@ -17,9 +17,20 @@ angular.module('ngd3.multiline', ['ngd3.services'])
             var xScale = SVG.xTimeScale;
             var yScale = SVG.yLinearScale;
 
+            var elemNode = d3.select('#'+id);
+
+            var line = d3.svg.line()
+                .interpolate("basis") // for curved lines
+                .x(function(d) { return xScale(d.x); })
+                .y(function(d) { return yScale(d.y); });
+
             $scope.$watch(SVG.dataScope, function(data) {
 
                 if(!data) return;
+
+                SVG = graph.getSVG($element);
+                xScale = SVG.xTimeScale;
+                yScale = SVG.yLinearScale;
 
                 var domains = graph.getDomains(data);
 
@@ -39,28 +50,23 @@ angular.module('ngd3.multiline', ['ngd3.services'])
                 var color = d3.scale.category10();
                 color.domain(lines.map(function(ln) { return ln.title; }));
 
-                var line = d3.svg.line()
-                    .interpolate("basis") // for curved lines
-                    .x(function(d) { return xScale(d.x); })
-                    .y(function(d) { return yScale(d.y); });
-
                 xScale.domain(domains.x);
                 yScale.domain(domains.y);
 
-                var elemNode = d3.select('#'+id);
                 var items = elemNode.selectAll(".item")
-                    .data(lines)
-                    .enter().append("g")
+                    .data(lines).enter().append("g")
                     .attr("class", "item")
                     .attr("transform", "translate(" + SVG.xMargin + "," + SVG.yMargin + ")");
 
+                items = elemNode.selectAll(".item");
+
                 items.append("path")
                     .attr("class", "line")
-                    .style("stroke", function(d) { return color(d.title); })
+                    .style("stroke", function(d) { return color(d.title); });
+
                 items.append("text");
 
-                items
-                    .select("path")
+                items.select("path")
                     .transition()
                     .attr("d", function(d) {
                         return line(d.points); 
