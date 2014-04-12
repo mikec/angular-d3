1919d4c9e7374c2729d6a08adc88a1635c48964f
Mike Calvanese <mfcalvanese@gmail.com>
2014-04-12 18:47:28 -0400

    added grunt copy for demo required libs

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,25 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
 
+        pkg: grunt.file.readJSON('package.json'),
+
+        copy: {
+            demo_required_js: {
+                src: [
+                    'bower_components/angular/angular.js',
+                    'bower_components/d3/d3.js',
+                    'bower_components/ui-router/release/angular-ui-router.js'
+                ],
+                dest: 'demo/lib/',
+                flatten: true,
+                expand: true
+            }
+        }
+
     });
 
-    grunt.registerTask('default', []);
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('default', ['copy:demo_required_js']);
 
 };
