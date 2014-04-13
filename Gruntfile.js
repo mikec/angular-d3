ebb8d8dfc442dd795c57165f56cfcaa24faa1c8a
Mike Calvanese <mfcalvanese@gmail.com>
2014-04-13 17:08:15 -0400

    split modules into different dist files

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,10 @@ module.exports = function(grunt) {
                     'bower_components/angular/angular.js',
                     'bower_components/d3/d3.js',
                     'bower_components/ui-router/release/angular-ui-router.js',
-                    'dist/angular-d3.js'
+                    'dist/angular-d3.js',
+                    'dist/angular-d3-axis.js',
+                    'dist/angular-d3-bars.js',
+                    'dist/angular-d3-lines.js',
                 ],
                 dest: 'demo/lib/',
                 flatten: true,
@@ -19,16 +22,25 @@ module.exports = function(grunt) {
         },
 
         concat: {
-            angular_d3_js: {
+            angular_d3_core_js: {
                 src: [
                     'src/ngd3.js',
                     'src/services.js',
-                    'src/directives.js',
-                    'src/axis/axis.js',
-                    'src/bars/bars.js',
-                    'src/lines/lines.js'
+                    'src/directives.js'
                 ],
                 dest: 'dist/angular-d3.js'
+            },
+            angular_d3_axis_js: {
+                src: ['src/axis/axis.js'],
+                dest: 'dist/angular-d3-axis.js'
+            },
+            angular_d3_bars_js: {
+                src: ['src/bars/bars.js'],
+                dest: 'dist/angular-d3-bars.js'
+            },
+            angular_d3_lines_js: {
+                src: ['src/lines/lines.js'],
+                dest: 'dist/angular-d3-lines.js'
             }
         },
 
@@ -46,8 +58,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-gh-pages');
 
     grunt.registerTask('default', [
-        'concat:angular_d3_js',
-        'copy:demo_required_js'
+        'concat',
+        'copy'
     ]);
 
 };
