9a8d724d7e01e00e41a809e4d29b676513cb9602
Mike Calvanese <mfcalvanese@gmail.com>
2014-05-17 00:15:51 -0400

    added controller and view code
    and some styles

--- demo/partials/guide_code/x-axis.js
+++ demo/partials/guide_code/x-axis.js
@@ -1,9 +1,12 @@
 angular.module('guide.xAxis', [])
 
 .controller('xAxisCtrl', ['$scope', function($scope) {
-    
+    // get today's date
     var d1 = new Date();
-    var d2 = new Date(d1.getTime() + 365 * 24 * 60 * 60 * 1000);
-    $scope.xDomain = [d1, d2];
 
+    // get the date 1 year from today
+    var d2 = new Date(d1.getTime() + 365 * 24 * 60 * 60 * 1000);
+
+    // set the domain for the X axis to a range from d1 to d2
+    $scope.xDomain = [d1, d2];
 }]);
